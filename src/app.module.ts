5d8805b53bf23e5e99c6810fec820f2b3e2777d6
Yerkhan Baigal <baigal.erhan@gmail.com>
2022-08-10 10:52:35 +0600

    remove appService, add ping endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import {Module} from '@nestjs/common'
 import {AppController} from './app.controller'
-import {AppService} from './app.service'
 import {CanvasModule} from './canvas/canvas.module'
 
 @Module({
 	imports: [CanvasModule],
 	controllers: [AppController],
-	providers: [AppService],
 })
 export class AppModule {}
